aa0a5d1b232ac423ad4279e7c9ac5fd52a0fa7b0
luddwichr <luddwichr@users.noreply.github.com>
2019-05-15 23:15:14 +0200

    WIP: implement placement check for corners matching

--- src/main/java/com/github/luddwichr/triominos/board/Board.java
+++ src/main/java/com/github/luddwichr/triominos/board/Board.java
@@ -28,17 +28,24 @@ public class Board {
 		if (isNotAdjacentToExistingPlacement(placement)) {
 			return false;
 		}
-		return leftNeighborFits(placement) && rightNeighborFits(placement) && middleNeighborFits(placement);
+		if (!(leftNeighborFits(placement) && rightNeighborFits(placement) && middleNeighborFits(placement))) {
+			return false;
+		}
+		return rightCornerFits(placement) && leftCornerFits(placement) && middleCornerFits(placement);
+	}
+
+	private boolean isFirstTileLocationCentered(Placement placement) {
+		return placement.getLocation().equals(UP_CENTER_TILE_LOCATION) || placement.getLocation().equals(DOWN_CENTER_TILE_LOCATION);
+	}
+
+	private boolean isAlreadyTaken(Placement placement) {
+		return placements.get(placement.getLocation()) != null;
 	}
 
 	private boolean isNotAdjacentToExistingPlacement(Placement placement) {
 		return !placements.containsKey(placement.getLocation().getLeftNeighbor())
 				&& !placements.containsKey(placement.getLocation().getRightNeighbor())
 				&& !placements.containsKey(placement.getLocation().getMiddleNeighbor());
-	}
-
-	private boolean isAlreadyTaken(Placement placement) {
-		return placements.get(placement.getLocation()) != null;
 	}
 
 	private boolean leftNeighborFits(Placement placement) {
@@ -60,8 +67,49 @@ public class Board {
 				.orElse(true);
 	}
 
-	private boolean isFirstTileLocationCentered(Placement placement) {
-		return placement.getLocation().equals(UP_CENTER_TILE_LOCATION) || placement.getLocation().equals(DOWN_CENTER_TILE_LOCATION);
+	private boolean rightCornerFits(Placement placement) {
+		// TODO: write test for all edge cases, refactor
+		int rightCorner = placement.getRightCorner();
+		Placement twoToTheRight = placements.get(placement.getLocation().getRightNeighbor().getRightNeighbor());
+		if (twoToTheRight != null && twoToTheRight.getLeftCorner() != rightCorner) {
+			return false;
+		}
+		Placement twoToTheRightOfMiddle = placements.get(placement.getLocation().getMiddleNeighbor().getRightNeighbor().getRightNeighbor());
+		if (twoToTheRightOfMiddle != null && twoToTheRightOfMiddle.getLeftCorner() != rightCorner) {
+			return false;
+		}
+		Placement oneToTheRightOfMiddle = placements.get(placement.getLocation().getMiddleNeighbor().getRightNeighbor());
+		return oneToTheRightOfMiddle == null || oneToTheRightOfMiddle.getMiddleCorner() == rightCorner;
+	}
+
+	private boolean leftCornerFits(Placement placement) {
+		// TODO: write test for all edge cases, refactor
+		int leftCorner = placement.getLeftCorner();
+		Placement twoToTheLeft = placements.get(placement.getLocation().getLeftNeighbor().getLeftNeighbor());
+		if (twoToTheLeft != null && twoToTheLeft.getRightCorner() != leftCorner) {
+			return false;
+		}
+		Placement twoToTheLeftOfMiddle = placements.get(placement.getLocation().getMiddleNeighbor().getLeftNeighbor().getLeftNeighbor());
+		if (twoToTheLeftOfMiddle != null && twoToTheLeftOfMiddle.getRightCorner() != leftCorner) {
+			return false;
+		}
+		Placement oneToTheLeftOfMiddle = placements.get(placement.getLocation().getMiddleNeighbor().getLeftNeighbor());
+		return oneToTheLeftOfMiddle == null || oneToTheLeftOfMiddle.getMiddleCorner() == leftCorner;
+	}
+
+	private boolean middleCornerFits(Placement placement) {
+		// TODO: write tests, refactor
+		int middleCorner = placement.getMiddleCorner();
+		Placement leftToOpposite = placements.get(placement.getLocation().getLeftNeighbor().getMiddleNeighbor());
+		if (leftToOpposite != null && leftToOpposite.getRightCorner() != middleCorner) {
+			return false;
+		}
+		Placement rightToOpposite = placements.get(placement.getLocation().getRightNeighbor().getMiddleNeighbor());
+		if (rightToOpposite != null && rightToOpposite.getLeftCorner() != middleCorner) {
+			return false;
+		}
+		Placement opposite = placements.get(placement.getLocation().getLeftNeighbor().getMiddleNeighbor().getRightNeighbor());
+		return opposite == null || opposite.getMiddleCorner() == middleCorner;
 	}
 
 	public Collection<Placement> getTilePlacements() {
--- src/test/java/com/github/luddwichr/triominos/board/BoardTest.java
+++ src/test/java/com/github/luddwichr/triominos/board/BoardTest.java
@@ -107,28 +107,65 @@ class BoardTest {
 	@Test
 	void isValidPlacementFormingHexagon() {
 		createIncompleteHexagon();
-		Placement sixthPlacement = new Placement(new Tile(0, 3, 4), Orientation.BAC, new Location(0, -1));
-		assertThat(board.isValidPlacement(sixthPlacement)).isTrue();
+		Placement placement = new Placement(new Tile(0, 3, 4), Orientation.BAC, new Location(0, -1));
+		assertThat(board.isValidPlacement(placement)).isTrue();
 	}
 
 	@Test
 	void isValidPlacementFormingInvalidHexagon() {
 		createIncompleteHexagon();
-		Placement sixthPlacement = new Placement(new Tile(0, 3, 5), Orientation.BAC, new Location(0, -1));
-		assertThat(board.isValidPlacement(sixthPlacement)).isFalse();
+		Placement placement = new Placement(new Tile(0, 3, 5), Orientation.BAC, new Location(0, -1));
+		assertThat(board.isValidPlacement(placement)).isFalse();
 	}
 
 	private void createIncompleteHexagon() {
-		Placement firstPlacement = new Placement(new Tile(0, 0, 4), Orientation.BCA, new Location(0, 0));
-		Placement secondPlacement = new Placement(new Tile(0, 0, 0), Orientation.ACB, new Location(-1, 0));
-		Placement thirdPlacement = new Placement(new Tile(0, 0, 1), Orientation.CAB, new Location(-1, -1));
-		Placement fourthPlacement = new Placement(new Tile(0, 1, 2), Orientation.CBA, new Location(-1, -2));
-		Placement fifthPlacement = new Placement(new Tile(0, 2, 3), Orientation.BCA, new Location(0, -2));
-		board.placeTile(firstPlacement);
-		board.placeTile(secondPlacement);
-		board.placeTile(thirdPlacement);
-		board.placeTile(fourthPlacement);
-		board.placeTile(fifthPlacement);
+		board.placeTile(new Placement(new Tile(0, 0, 4), Orientation.BCA, new Location(0, 0)));
+		board.placeTile(new Placement(new Tile(0, 0, 0), Orientation.ACB, new Location(-1, 0)));
+		board.placeTile(new Placement(new Tile(0, 0, 1), Orientation.CAB, new Location(-1, -1)));
+		board.placeTile(new Placement(new Tile(0, 1, 2), Orientation.CBA, new Location(-1, -2)));
+		board.placeTile(new Placement(new Tile(0, 2, 3), Orientation.BCA, new Location(0, -2)));
+	}
+
+	@Test
+	void isValidPlacementFormingBridgeMatchRightCorner() {
+		createIncompleteBridgeMissingRightCornerMatch();
+		Placement placement = new Placement(new Tile(3, 3, 4), Orientation.ABC, new Location(1, -1));
+		assertThat(board.isValidPlacement(placement)).isTrue();
+	}
+
+	@Test
+	void isValidPlacementFormingInvalidBridgeNotMatchingRightCorner() {
+		createIncompleteBridgeMissingRightCornerMatch();
+		Placement placement = new Placement(new Tile(3, 3, 5), Orientation.ABC, new Location(1, -1));
+		assertThat(board.isValidPlacement(placement)).isFalse();
+	}
+
+	private void createIncompleteBridgeMissingRightCornerMatch() {
+		createIncompleteHexagon();
+		board.placeTile(new Placement(new Tile(2, 2, 3), Orientation.BAC, new Location(0, -3)));
+		board.placeTile(new Placement(new Tile(2, 3, 3), Orientation.ABC, new Location(1, -3)));
+		board.placeTile(new Placement(new Tile(3, 3, 3), Orientation.ACB, new Location(1, -2)));
+	}
+
+	@Test
+	void isValidPlacementFormingBridgeMatchLeftCorner() {
+		createIncompleteBridgeMissingLeftCornerMatch();
+		Placement placement = new Placement(new Tile(3, 3, 4), Orientation.ABC, new Location(1, -1));
+		assertThat(board.isValidPlacement(placement)).isTrue();
+	}
+
+	@Test
+	void isValidPlacementFormingInvalidBridgeNotMatchingLeftCorner() {
+		createIncompleteBridgeMissingLeftCornerMatch();
+		Placement placement = new Placement(new Tile(5, 3, 4), Orientation.ABC, new Location(1, -1));
+		assertThat(board.isValidPlacement(placement)).isFalse();
+	}
+
+	private void createIncompleteBridgeMissingLeftCornerMatch() {
+		createIncompleteHexagon();
+		board.placeTile(new Placement(new Tile(0, 4, 4), Orientation.BAC, new Location(0, 1)));
+		board.placeTile(new Placement(new Tile(4, 4, 4), Orientation.ABC, new Location(1, 1)));
+		board.placeTile(new Placement(new Tile(3, 4, 4), Orientation.ACB, new Location(1, 0)));
 	}
 
 	private static Placement placementFacingUp(Location location) {

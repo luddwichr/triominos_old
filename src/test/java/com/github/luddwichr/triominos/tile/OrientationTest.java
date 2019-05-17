b852f05099cde9869089f6afe96304692d6778b5
luddwichr <luddwichr@users.noreply.github.com>
2019-05-17 08:41:02 +0200

    refactor test

--- src/test/java/com/github/luddwichr/triominos/tile/OrientationTest.java
+++ src/test/java/com/github/luddwichr/triominos/tile/OrientationTest.java
@@ -9,50 +9,43 @@ import static org.junit.jupiter.api.Assertions.*;
 class OrientationTest {
 
 	@Test
-	void abc() {
+	void isFacingUp() {
 		assertThat(Orientation.ABC.isFacingUp()).isTrue();
-		assertThat(Orientation.ABC.getLeft()).isEqualTo(Corner.A);
-		assertThat(Orientation.ABC.getMiddle()).isEqualTo(Corner.B);
-		assertThat(Orientation.ABC.getRight()).isEqualTo(Corner.C);
-	}
-
-	@Test
-	void acb() {
 		assertThat(Orientation.ACB.isFacingUp()).isFalse();
-		assertThat(Orientation.ACB.getLeft()).isEqualTo(Corner.A);
-		assertThat(Orientation.ACB.getMiddle()).isEqualTo(Corner.C);
-		assertThat(Orientation.ACB.getRight()).isEqualTo(Corner.B);
-	}
-
-	@Test
-	void cab() {
 		assertThat(Orientation.CAB.isFacingUp()).isTrue();
-		assertThat(Orientation.CAB.getLeft()).isEqualTo(Corner.C);
-		assertThat(Orientation.CAB.getMiddle()).isEqualTo(Corner.A);
-		assertThat(Orientation.CAB.getRight()).isEqualTo(Corner.B);
-	}
-
-	@Test
-	void cba() {
 		assertThat(Orientation.CBA.isFacingUp()).isFalse();
-		assertThat(Orientation.CBA.getLeft()).isEqualTo(Corner.C);
-		assertThat(Orientation.CBA.getMiddle()).isEqualTo(Corner.B);
-		assertThat(Orientation.CBA.getRight()).isEqualTo(Corner.A);
-	}
-
-	@Test
-	void bca() {
 		assertThat(Orientation.BCA.isFacingUp()).isTrue();
-		assertThat(Orientation.BCA.getLeft()).isEqualTo(Corner.B);
-		assertThat(Orientation.BCA.getMiddle()).isEqualTo(Corner.C);
-		assertThat(Orientation.BCA.getRight()).isEqualTo(Corner.A);
+		assertThat(Orientation.BAC.isFacingUp()).isFalse();
 	}
 
 	@Test
-	void bac() {
-		assertThat(Orientation.BAC.isFacingUp()).isFalse();
+	void getLeft() {
+		assertThat(Orientation.ABC.getLeft()).isEqualTo(Corner.A);
+		assertThat(Orientation.ACB.getLeft()).isEqualTo(Corner.A);
+		assertThat(Orientation.CAB.getLeft()).isEqualTo(Corner.C);
+		assertThat(Orientation.CBA.getLeft()).isEqualTo(Corner.C);
+		assertThat(Orientation.BCA.getLeft()).isEqualTo(Corner.B);
 		assertThat(Orientation.BAC.getLeft()).isEqualTo(Corner.B);
-		assertThat(Orientation.BAC.getMiddle()).isEqualTo(Corner.A);
+	}
+
+	@Test
+	void getRight() {
+		assertThat(Orientation.ABC.getRight()).isEqualTo(Corner.C);
+		assertThat(Orientation.ACB.getRight()).isEqualTo(Corner.B);
+		assertThat(Orientation.CAB.getRight()).isEqualTo(Corner.B);
+		assertThat(Orientation.CBA.getRight()).isEqualTo(Corner.A);
+		assertThat(Orientation.BCA.getRight()).isEqualTo(Corner.A);
 		assertThat(Orientation.BAC.getRight()).isEqualTo(Corner.C);
 	}
+
+	@Test
+	void getMiddle() {
+		assertThat(Orientation.ABC.getMiddle()).isEqualTo(Corner.B);
+		assertThat(Orientation.ACB.getMiddle()).isEqualTo(Corner.C);
+		assertThat(Orientation.CAB.getMiddle()).isEqualTo(Corner.A);
+		assertThat(Orientation.CBA.getMiddle()).isEqualTo(Corner.B);
+		assertThat(Orientation.BCA.getMiddle()).isEqualTo(Corner.C);
+		assertThat(Orientation.BAC.getMiddle()).isEqualTo(Corner.A);
+	}
+
 }
